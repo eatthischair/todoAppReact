6d72028cdc1b31650499f624c149d4401aa4a0c2
eatthischair <based.god.swag.od@gmail.com>
2025-01-10 15:33:03 -0600

    centralized icons

--- src/mui/index.js
+++ src/mui/index.js
@@ -5,6 +5,11 @@ import OpenWithIcon from '@mui/icons-material/OpenWith';
 import CircularProgress from '@mui/material/CircularProgress';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import LinearProgress from '@mui/material/LinearProgress';
+import UndoIcon from '@mui/icons-material/Undo';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import CheckIcon from '@mui/icons-material/Check';
 
 export {
   DoneIcon,
@@ -14,4 +19,9 @@ export {
   CircularProgress,
   AddIcon,
   DeleteIcon,
+  LinearProgress,
+  UndoIcon,
+  RepeatIcon,
+  ModeEditIcon,
+  CheckIcon,
 };
--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,13 +1,25 @@
 import { useState, useEffect } from 'react';
-import UndoIcon from '@mui/icons-material/Undo';
-import RepeatIcon from '@mui/icons-material/Repeat';
-import DeleteIcon from '@mui/icons-material/Delete';
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import OpenWithIcon from '@mui/icons-material/OpenWith';
-import CheckIcon from '@mui/icons-material/Check';
-import LinearProgress from '@mui/material/LinearProgress';
+// import UndoIcon from '@mui/icons-material/Undo';
+// import RepeatIcon from '@mui/icons-material/Repeat';
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import ModeEditIcon from '@mui/icons-material/ModeEdit';
+// import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+// import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+// import OpenWithIcon from '@mui/icons-material/OpenWith';
+// import CheckIcon from '@mui/icons-material/Check';
+// import LinearProgress from '@mui/material/LinearProgress';
+
+import {
+  UndoIcon,
+  RepeatIcon,
+  DeleteIcon,
+  ModeEditIcon,
+  CalendarMonthIcon,
+  ArrowUpwardIcon,
+  OpenWithIcon,
+  CheckIcon,
+  LinearProgress,
+} from '../mui/index.js';
 
 import { useTaskContext } from '../context/TaskContext';
 import { useNavigate } from 'react-router-dom';
